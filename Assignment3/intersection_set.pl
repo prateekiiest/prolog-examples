29f492c4aae70bf8a139c58848ba978e873cb083
Prateek Chanda <prateekkol21@gmail.com>
2019-02-07 20:34:12 +0530

    Create intersection_set.pl

---
+++ Assignment3/intersection_set.pl
@@ -0,0 +1,9 @@
+our_member(X,[X|Rest]).
+our_member(X,[Y|Rest]):-our_member(X,Rest).
+
+intersection([],L,[]).
+intersection([X|L1],L2,[X|L3]):- our_member(X,L2),!,
+    
+    intersection(L1,L2,L3).
+intersection([X|L1],L2,L3):- intersection(L1,L2,L3).
+
