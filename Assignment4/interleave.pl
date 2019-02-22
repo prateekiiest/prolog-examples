8c9285327563346ea3ddf66c64351f0dce399a83
Prateek Chanda <prateekkol21@gmail.com>
2019-02-22 10:33:30 +0530

    Update interleave.pl

--- Assignment4/interleave.pl
+++ Assignment4/interleave.pl
@@ -1,12 +1,14 @@
-interleave([X],[],[X]).
+list_length([],0).
+list_length([H|T],N):-list_length(T,N1), N is N1+1.
 
+interleave([X],[],[X]).
 interleave([X|L1],[Y|L2],[X|[Y|L3]]):-
 interleave(L1,L2,L3).
 
-inter([],[],[]).
-inter(L1,L2,L3):-
-length3(L1,N1),
-length3(L2,N2),
+interleave([],[],[]).
+interleave(L1,L2,L3):-
+list_length(L1,N1),
+list_length(L2,N2),
 N1>=N2,
 interleave(L1,L2,L3);
 interleave(L2,L1,L3).
