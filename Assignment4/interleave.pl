dd07820bb9a990f9d046cf30be3600a85baa178d
Prateek Chanda <prateekkol21@gmail.com>
2019-02-22 10:36:39 +0530

    Update interleave.pl

--- Assignment4/interleave.pl
+++ Assignment4/interleave.pl
@@ -9,7 +9,7 @@ interleave([],[],[]).
 interleave(L1,L2,L3):-
 list_length(L1,N1),
 list_length(L2,N2),
-N1>=N2,
+N1>=N2,  % if the length of the first list is greater than that of the second list, then interleave the first into the second else otherwise
 interleave(L1,L2,L3);
 interleave(L2,L1,L3).
 
