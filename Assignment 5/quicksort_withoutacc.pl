15f1ae3ef26bfd2ba7ca56af795d602df6b32744
Prateek Chanda <prateekkol21@gmail.com>
2019-03-14 19:19:35 +0530

    Create quicksort_withoutacc.pl

---
+++ Assignment 5/quicksort_withoutacc.pl
@@ -0,0 +1,6 @@
+pivot(_, [], [], []).
+pivot(Pivot, [Head|Tail], [Head|LessOrEqualThan], GreaterThan) :- Pivot >= Head, pivot(Pivot, Tail, LessOrEqualThan, GreaterThan). 
+pivot(Pivot, [Head|Tail], LessOrEqualThan, [Head|GreaterThan]) :- pivot(Pivot, Tail, LessOrEqualThan, GreaterThan).
+
+quicksort([], []).
+quicksort([Head|Tail], Sorted) :- pivot(Head, Tail, List1, List2), quicksort(List1, SortedList1), quicksort(List2, SortedList2), append(SortedList1, [Head|SortedList2], Sorted).
