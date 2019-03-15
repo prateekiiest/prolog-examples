d34d356505caa7da18d97c072499cce371a0d0fc
Prateek Chanda <prateekkol21@gmail.com>
2019-03-15 10:32:59 +0530

    Update bubble_sort.pl

--- Assignment 5/bubble_sort.pl
+++ Assignment 5/bubble_sort.pl
@@ -1,14 +1,14 @@
 bubblesort([],[]).
 bubblesort(In, [H|Out]):-
-  pulldown(In, [H|T]),
+  order(In, [H|T]),
   bubblesort(T, Out).
 
-pulldown([],[]).
-pulldown([A], [A]).
-pulldown([H|T], [H,X|Y]):-
-  pulldown(T,[X|Y]),
+order([],[]).
+order([A], [A]).
+order([H|T], [H,X|Y]):-
+  order(T,[X|Y]),
   H =< X.
 
-pulldown([H|T], [X,H|Y]):-
-  pulldown(T,[X|Y]),
+order([H|T], [X,H|Y]):-
+  order(T,[X|Y]),
   H > X.
