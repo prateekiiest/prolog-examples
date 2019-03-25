d796c1b5a27f793b3781b62b6dbe8acde9f51e2a
Prateek Chanda <prateekkol21@gmail.com>
2019-03-25 19:21:04 +0530

    Create insert_bst.pl

---
+++ Assignment 6/insert_bst.pl
@@ -0,0 +1,9 @@
+insert_bst(X, nil, bst(nil,	X,bil)).
+insert_bst(X,bst(Left, Root, Right),bst(Left1, Root, Right)):-
+    X =< Root,
+    !,
+    insert(X,Left, Left1).
+
+insert_bst(X, bst(Left,Root, Right),bst(Left,Root,Right1)):-
+    X> Root,
+    insert_bst(X,Right,Right1).
