8d776fe1b2572c6a943aadeada0ea3b6dfc8571c
Prateek Chanda <prateekkol21@gmail.com>
2019-04-05 11:34:21 +0530

    Update post_order_from_pre_in.pl

--- Assignment 6/post_order_from_pre_in.pl
+++ Assignment 6/post_order_from_pre_in.pl
@@ -1,3 +1,14 @@
+postorder(nil,[]).
+postorder(tree(X,Left,Right),Traversal):-
+	postorder(Left,Left_t),
+	postorder(Right,Right_t),
+	my_append(Left_t,Right_t,Temp),
+	my_append(Temp,[X],Traversal).
+
+my_append([],List,List).
+my_append([Head1|List1],List2,[Head1|List3]):-
+        my_append(List1,List2,List3).
+
 post_order(Pre,In,Post):-
 	construct(Pre,In,Tree),
 	postorder(Tree,Post).
