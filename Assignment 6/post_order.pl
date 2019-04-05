c968c45f56d386b7787a5ae2b6f3aca0613125b9
Prateek Chanda <prateekkol21@gmail.com>
2019-04-05 10:02:51 +0530

    Update post_order.pl

--- Assignment 6/post_order.pl
+++ Assignment 6/post_order.pl
@@ -1,5 +1,10 @@
-post_order(nil,[]).
-post_order(bt(Left,Root, Right), List):-
-    post_order(Left, List1),
-    post_order(Right, List2),
-    append(List1,[List2|Root], List).
+postorder(nil,[]).
+postorder(tree(X,Left,Right),Traversal):-
+	postorder(Left,Left_t),
+	postorder(Right,Right_t),
+	my_append(Left_t,Right_t,Temp),
+	my_append(Temp,[X],Traversal).
+
+my_append([],List,List).
+my_append([Head1|List1],List2,[Head1|List3]):-
+        my_append(List1,List2,List3).
